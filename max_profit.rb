03f62b5edaa050ab85b1820667e13af59a13c1c1
agdavid <antonio.g.david@gmail.com>
2016-10-20 14:38:56 -0400

    raise error if less than two items in hash

--- max_profit.rb
+++ max_profit.rb
@@ -8,3 +8,13 @@
 
 # Write an efficient function that takes stock_prices_yesterday and returns the best profit 
 # I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
+
+def max_profit(hash)
+
+  if hash.length < 2
+    # raise error if there are less than two prices b/c you need to "buy" and "sell" at two prices to profit
+    raise "Oops! You need at least two prices to calculate a profit."
+  else
+
+  end
+end
