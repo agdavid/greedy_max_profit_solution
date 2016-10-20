e302b640a53785b41eceebb7981a298f3ccf75ab
agdavid <antonio.g.david@gmail.com>
2016-10-20 15:08:31 -0400

    add loop to test prices

--- max_profit.rb
+++ max_profit.rb
@@ -9,7 +9,9 @@
 # Write an efficient function that takes stock_prices_yesterday and returns the best profit 
 # I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
 
-def max_profit(hash)
+# hash1 = { 0=>15, 1=>35, 2=>7, 3=>18, 4=>28, 5=>1 } #max_profit expected is (35-15) = 20
+
+def get_max_profit(hash)
 
     if hash.length < 2
         # raise error if there are less than two prices b/c you need to "buy" and "sell" at two prices to profit
@@ -19,6 +21,23 @@ def max_profit(hash)
         min_price = hash[0]
         # the first max_profit will be the difference between the first and second prices of the day
         # why? these are the first opportunities to "buy" and "sell"
-        max_profit = hash[1] - hash[0]
+        max_profit = (hash[1]-hash[0])
+
+        # loop through entire hash and update max_profit and min_price accordingly
+        hash.each_with_index do |current_price, index|
+            # skip the first key-value pair b/c you already have that calculated as the defaults
+            if index == 0 then next end
+                
+            # calculate profit if sold at current price    
+            potential_profit = (current_price-min_price)
+
+            # keep the higher profit
+            max_profit = [max_profit, potential_profit].max
+
+            # keep the lower price
+            min_price = [min_price, current_price].min 
+        end
+
+        return max_profit
     end
 end
